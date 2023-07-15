5827f673cd09796f26984f6a789f64f41a533623
O Hung Lun <hunglun.o@gmail.com>
2023-07-15 15:17:21 +0000

    completed part 1

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Header(){
+    return (
+      <header>
+         <title>Keeper App</title>
+         <h1>Keeper</h1>
+      </header>
+    );
+  }
+
+  export default Header;
