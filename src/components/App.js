f930d7a802cf8147c713e37e98da00e11c2084b0
O Hung Lun <hunglun.o@gmail.com>
2023-07-17 03:40:53 +0000

    implemented note delete function

--- src/components/App.js
+++ src/components/App.js
@@ -18,16 +18,41 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import React from "react";
 function App() {
+  const [ _notes, setNotes] = React.useState(notes);
+  function deleteNote(id){
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem) => {
+        return id !== noteItem.key;
+      });
+    });
+  }
   return (
     <div className="App">
       <Header className="App-header">
       </Header>
-      {notes.map(note => (
-      <Note key={note.key} title={note.title} content={note.content} />))}
+      {_notes.map(note => (
+      <Note onDelete={deleteNote} id={note.key} key={note.key} title={note.title} content={note.content} />))}
       <Footer />
     </div>
   );
 }
+
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+
+//2. Implement the delete note functionality. [DONE]
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
 
 export default App;
--- src/components/Note.js
+++ src/components/Note.js
@@ -2,10 +2,15 @@
 //<h1> for a title and a <p> for the content.
 import React from "react";
 function Note(props){
+    function deleteNote(event){
+        props.onDelete(props.id);
+        event.preventDefault(); 
+    }
     return (
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button onClick={deleteNote} > DELETE</button>
         </div>
     );
 }
